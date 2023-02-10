59f3eb85ae0db131f98318b9868855c70b34dbc2
Adaeze Chukwukereze <adaeze633@gmail.com>
2023-02-10 07:55:54 +0100

    upload

---
+++ apis/views.py
@@ -0,0 +1,129 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, APIView
+from rest_framework.response import Response
+from .serializers import NewsSerializer
+from blog.models import News
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+# generics view
+class API_LIST_PAGE(ListCreateAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+
+class API_DETAIL_PAGE(RetrieveUpdateDestroyAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+
+
+
+
+@api_view(["GET", "POST"])         # FUNCTION-BASED
+def api_list_page(request):
+    if request.method == "GET":
+        news = News.objects.all()
+        serializer = NewsSerializer(news, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    elif request.method == "POST":
+        serializer = NewsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTPP_200_OK)
+
+
+
+
+class Api_list_page(APIView):   # CLASS BASED
+    def get(self, request):
+        news = News.objects.all()
+        serializer = NewsSerializer(news, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = NewsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTPP_200_OK)
+
+
+
+@api_view(["GET", "PUT"])
+def api_detail_page(request, slug): 
+    try:
+        news = News.objects.get(slug=slug)
+    except News.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == "GET":
+        serializer = NewsSerializer(news)
+        return Response(serializer.data)
+
+    elif request.metthod == "PUT":
+        serializer = NewsSerializer(news, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == "DELETE":
+        news.delete()
+        return Response(status=status.HTTP_200_OK)
+
+
+class Api_detail_view(APIView):
+    def get(self, request, slug):
+        news = News.objects.get(slug=slug)
+        serializer = NewsSerializer(news)
+        return Response(serializer.data)
+
+
+@api_view(["POST"])
+def api_create_page(request):
+    serializer = NewsSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class Api_create_page(APIView):
+    def post(self, request):
+        serializer = NewsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["PUT"])
+def api_update_page(request, slug):
+    try:
+        news = News.objects.get(slug=slug)
+    except News.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    serializer = NewsSerializer(news, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
